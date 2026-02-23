cf18893effd16c8eefd7000cdee083166d6db14e
Sih-Jin Lee <163836575+Lee-SihJin@users.noreply.github.com>
2026-02-23 15:43:53 +0200

    4

--- OnlineCV/OnlineCV/obj/Debug/net10.0/OnlineCV.AssemblyInfo.cs
+++ OnlineCV/OnlineCV/obj/Debug/net10.0/OnlineCV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineCV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8019e15604fab3fbe2be0db43f1bb9503553478")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f7e725270e567be7588bd11105d62faec1d472c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineCV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineCV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
